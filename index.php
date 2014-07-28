ffd649a9450947c10560066eed7e0bb34d17bd1a
LorenzoPaciotti <lupacche@gmail.com>
2014-07-28 21:01:34 +0200

    init

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>DMIPrePrints</title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+    </head>
+    <body>
+        <?php
+        //echo 'testphp';
+        //include 'auth.php';
+        //echo '</br></br>Script auth concluso';
+        include 'login.php';
+        ?>
+    </body>
+</html>
---
+++ login.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>DMIPrePrints</title>
+        <link href="css/index.css" rel="stylesheet" type="text/css">
+        <link href="css/controlli.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+                <div class="contenitore1">
+            <form name="form_login" method="post" action="">
+                <div class="contenitore2">
+                    
+                    <div class="input">
+                        <p>
+                            Username
+                            <input name="nome" type="text" class="textbox" value=""/>
+                            Password
+                            <input name="password" type="password" class="textbox" value="">
+                        </p>
+                        <input class="bottoneLogin" name="invia" type="submit" value="Invia"/>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </body>
+</html>
