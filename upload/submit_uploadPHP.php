4417c8f452d95ec587f5b8a1c70ad1e0813323c0
LorenzoPaciotti <lupacche@gmail.com>
2014-07-29 18:11:34 +0200

    fix

--- upload/submit_uploadPHP.php
+++ upload/submit_uploadPHP.php
@@ -6,8 +6,6 @@ $fileTmpLoc = $_FILES["userfile"]["tmp_name"];
 $pathAndName = "/home/lorenzo/uploads/".$fileName;
 // Run the move_uploaded_file() function here
 $moveResult = move_uploaded_file($fileTmpLoc, $pathAndName);
-echo "$fileTmpLoc</br>";
-echo "$pathAndName</br>";
 print_r(error_get_last());
 echo "</br>";
 // Evaluate the value returned from the function if needed
