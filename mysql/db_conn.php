811b356bdae1dcbe63d8338e15888933916a23bf
LorenzoPaciotti <lupacche@gmail.com>
2014-07-30 15:09:25 +0200

    fix

--- mysql/db_conn.php
+++ mysql/db_conn.php
@@ -1,9 +1,11 @@
 <?php
 
+#VARIABILE GLOBALE PER LA CONNESSIONE AL DB, VIENE USATA DA TUTTI I FILE DB_*.PHP
 $db_connect;
 
+#CONNESSIONE SERVER DB
 function connettiDBManager() {
-    #CONNESSIONE SERVER DB
+    
     global $db_connect;
     $db_connect = new mysqli('localhost', 'root@localhost', null) or die('errore connessione mysql');
     $errore = mysqli_errno($db_connect);
--- mysql/db_insert.php
+++ mysql/db_insert.php
@@ -8,8 +8,13 @@ include 'db_conn.php';
 
 function inserisci_paper($titolo, $autore, $abstract, $file_path) {
     global $db_connect;
+    #db_conn.php -> connettiDBManager()
     connettiDBManager();
-
-    $query = 'INSERT INTO PRINTS(titolo,autore,abstract,percorso) values () ';
+    
+    #selezione dello schema DMIPrePrints
+    mysqli_select_db($db_connect,'DMIPrePrints') or die('Could not select database');
+    
+    $query = 'INSERT INTO PRINTS(titolo,autore,abstract,percorso) values ('.$titolo.','.$autore.','.$abstract.','.$file_path.')';
     $result = mysqli_query($db_connect, $query) or die('Query failed: ' . mysqli_error());
+    print $result;
 }
