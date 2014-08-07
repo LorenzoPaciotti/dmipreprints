26e58fca9e1943fd65b2c900b2b031fba411929f
lorenzo <lupacche@gmail.com>
2014-08-07 17:29:09 +0200

    ldap

--- authorization/auth.php
+++ authorization/auth.php
@@ -2,32 +2,36 @@
 
 function LDAPAuth($UID) {
 
-    $auth_result = false;
-
-// variabili connessione LDAP
-    $ldaphost = "192.168.141.130";  // your ldap servers
-    $ldapport = 389;                 // your ldap server's port number
+// variabili connessione LDAP DA STORARE SU CONF
+    $ldaphost = "192.168.158.128";  // indirizzo host LDAP
+    $ldapport = 389;                // porta su host LDAP
+    //
+    //
 // connessione a LDAP
     $ldapconn = ldap_connect($ldaphost, $ldapport) or die("Could not connect to $ldaphost");
     ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
 
     //TEST
     $ds = $ldapconn;
-    $dn = "dc=dmi,dc=unipg,dc=it";
+    $dn = "ou=users,dc=dmi,dc=unipg,dc=it";
     //$filter = "(|(sn=$person*)(givenname=$person*))"; ESEMPIO
-    $filter = "cn=" . $UID;
+    //l'utente deve avere la licenza all'uso di DMIPrePrints
+    $filter = "(&(carLicense=:dmipreprints:)(uid=$UID))";
     $justthese = array("ou", "sn", "givenname", "mail");
 
     $sr = ldap_search($ds, $dn, $filter, $justthese);
 
     $info = ldap_get_entries($ds, $sr);
-
-    echo $info["count"] . " entries returned\n";
+    echo $info["count"] . " OK!\n";
+    
+    if($info["count"] == 1){
+        return true;
+    }
 
     //ENDTEST
 
 
-    return $auth_result;
+    return false;
 }
 
 //RADIUS TEST
--- mysql/db_select.php
+++ mysql/db_select.php
@@ -15,9 +15,15 @@ function listaAnni() {
     return $result;
 }
 
-function interrogaPerAnno($anno) {
+function interrogaPerAnno($anno, $moderatore) {
     global $db_connect;
     $query = 'select * from PRINTS where anno=' . $anno;
+    
+    //presentiamo al pubblico solo i paper approvati dal moderatore
+    if (!$moderatore){
+        $query = $query.' AND approvato=1';
+    }
+    
     $query = pulisciQuery($query);
     echo $query;
     $result = mysqli_query($db_connect, $query) or die(mysqli_error($db_connect));
@@ -25,20 +31,32 @@ function interrogaPerAnno($anno) {
     return $result;
 }
 
-function interrogaPerKeyword($keyword) {
+function interrogaPerKeyword($keyword, $moderatore) {
     global $db_connect;
     $keyword = '"%' . $keyword . '%"';
     $query = 'select * from PRINTS where titolo LIKE ' . $keyword . ' OR abstract LIKE ' . $keyword . ' OR autore LIKE ' . $keyword;
-    $query = pulisciQuery($query);
+    
+    //presentiamo al pubblico solo i paper approvati dal moderatore
+    if (!$moderatore){
+        $query = $query.' AND approvato=1';
+    }
+    
+    $query = pulisciQuery($query, $moderatore);
     echo $query;
     $result = mysqli_query($db_connect, $query) or die(mysqli_error($db_connect));
     mysqli_close($db_connect);
     return $result;
 }
 
-function interrogaPerUID($uid) {
+function interrogaPerUID($uid, $moderatore) {
     global $db_connect;
     $query = 'select * from PRINTS where uid=' . $uid;
+    
+    //presentiamo al pubblico solo i paper approvati dal moderatore
+    if (!$moderatore){
+        $query = $query.' AND approvato=1';
+    }
+    
     $query = pulisciQuery($query);
     echo $query;
     $result = mysqli_query($db_connect, $query) or die(mysqli_error($db_connect));
