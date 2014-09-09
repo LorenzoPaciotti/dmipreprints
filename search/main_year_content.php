e67cfd684fc5e48a997bfd472ca84e543508ab3c
lorenzo <lupacche@gmail.com>
2014-09-09 10:19:01 +0200

    RELEASE_01

--- search/main_year_content.php
+++ search/main_year_content.php
@@ -2,7 +2,7 @@
 require $_SERVER['DOCUMENT_ROOT'] . '/dmipreprints/' . 'mysql/db_select.php';
 require_once 'main_tabella.php';
 if (isset($_POST['query']) && $_POST['query']!=''){
-    stampaTabella(interrogaPerAnno($_POST['query']),1);
+    stampaTabella(interrogaPerAnno($_POST['query'],0),1);
 }else{
     print_r ('Invalid query');
 }
